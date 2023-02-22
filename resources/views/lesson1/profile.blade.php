1f5a9ecb08a3d783990056bbd41bbfb5989ae34b
mahesuru.pasan <mahesuru.pasan@ethan-tech.jp>
2023-02-22 10:15:04 +0900

    1015

--- resources/views/lesson1/profile.blade.php
+++ resources/views/lesson1/profile.blade.php
@@ -1,8 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Ethan Technology Japan</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+<div class="container mt-2">
+<div class="row">
+<div class="col-lg-12 margin-tb">
+<div class="pull-left mb-2">
+<h3>Ethan Technology Japan</h3>
+</div>
 <h3> ご利用ありがとうございます。</h3>
   <br>
 
   <h4> メールを送信しました。 </h4>
   <h5> メールボックスをご確認の上、再度お問い合わせ頂きますようお願い申し上げます </h5>
-<a href="back"> 戻る </a>
 
+  <button href="back" class="btn btn-primary">戻る</button>
+
+
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
 
