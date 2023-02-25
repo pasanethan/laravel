499343b758ff4eb0375a393b9addd011305f59ca
Pasan <mahesuru.pasan@ethan-tech.jp>
2023-02-25 10:31:13 +0900

    copy

---
+++ resources/views/companies copy/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Mail List</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+<div class="container mt-2">
+<div class="row">
+<div class="col-lg-12 margin-tb">
+<div>
+<h2 style="text-align:center">Welcome</h2>
+<h5>メール一覧</h5>
+</div>
+<div class="pull-right mb-2">
+<a class="btn btn-success" href="{{ route('companies.create') }}"> 登録</a>
+<a class="btn btn-success"> 送信</a>
+</div>
+</div>
+</div>
+@if ($message = Session::get('success'))
+<div class="alert alert-success">
+<p>{{ $message }}</p>
+</div>
+@endif
+
+<table class="table table-bordered">
+<tr>
+<th></th>
+<th>名前</th>
+<th>メールアドレス</th>
+<th>都道府県 </th>
+<th width="150px">変更一覧</th>
+</tr>
+@foreach ($companies as $company)
+<tr>
+<td><input type="checkbox" value="{{ $company->id }}" name="checkbox_value[]"></td>
+<td>{{ $company->name }}</td>
+<td>{{ $company->email }}</td>
+<td>{{ $company->address }}</td>
+<td>
+<form action="{{ route('companies.destroy',$company->id) }}" method="Post">
+<a class="btn btn-primary" href="{{ route('companies.edit',$company->id) }}">変更</a>
+@csrf
+@method('DELETE')
+<button type="submit" class="btn btn-danger">削除</button>
+</form>
+</td>
+</tr>
+@endforeach
+</table>
+{!! $companies->links() !!}
+</body>
+</html>
