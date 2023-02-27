dc839414678e27ec839666001c19f0529489ff77
mahesuru.pasan <mahesuru.pasan@ethan-tech.jp>
2023-02-27 11:47:54 +0900

    1148

--- app/Http/Controllers/CompanyCRUDController2.php
+++ app/Http/Controllers/CompanyCRUDController2.php
@@ -16,7 +16,7 @@ class CompanyCRUDController2 extends Controller
     public function index()
     {
 
-        $data['companies'] = Company::orderBy('id', 'desc')->paginate(5);
+        $data['companies'] = Company::orderBy('id', 'desc')->paginate(20);
         return view('companies2.index', $data);
     }
 /**
